84fb255960f8c54b94a35bbbfee27892d3be59f4
Anders Wikum <aew236@cornell.edu>
2023-07-14 15:54:52 -0700

    Added code for analysis of DP computation tree

--- obm_dp.py
+++ obm_dp.py
@@ -1,18 +1,6 @@
-from itertools import chain, combinations
 from params import _Array
-from typing import Optional
+from util import powerset, diff
 import numpy as np
-
-
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    powerset = chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-    return list(map(set, powerset))
-
-
-def diff(S: frozenset, u: int) -> frozenset:
-    return S.difference(set([u]))
 
 
 def cache_stochastic_opt(A: _Array, p: _Array) -> dict:
