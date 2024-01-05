Fixed precision issues with LP approximation

algorithms.py
@@ -249,7 +249,19 @@ def lp_match(A: _Array, p: _Array, verbose: Optional[bool] = False) -> _Array:
 
 
 def _compute_proposal_probs(x, p):
-    return np.maximum(np.minimum(x / _x_normalizing_constant(x, p), 1), 0)
+    eps = np.finfo(np.float32).eps
+    
+    denom_matrix = _x_normalizing_constant(x, p)
+    proposal_probs = x / denom_matrix
+    
+    proposal_probs = np.where(denom_matrix > eps, proposal_probs, 0)
+    proposal_probs = np.where(proposal_probs <= 1, proposal_probs, 1)
+    proposal_probs = np.round(proposal_probs, 5)
+    
+    assert (np.all(proposal_probs >= 0)), f"{proposal_probs[proposal_probs < 0]}"
+    assert (np.all(proposal_probs <= 1)), f"{proposal_probs[proposal_probs > 1]}"
+    
+    return proposal_probs
 
 def _vec_binomial(p: _Array):
     return np.array([np.random.binomial(1, pt) for pt in p]).astype(bool)
@@ -272,20 +284,19 @@ def online_lp_rounding(x, A, p, coin_flips):
                 matching.append((t, matched_node))
                 val += A[t, matched_node]
                 offline_mask[matched_node] = 0
-                valid_proposals[matched_node] = 0
-                rejections = _vec_binomial(n * [p[t]])
-                new_rejections = np.bitwise_and(
-                    rejections,
-                    valid_proposals
-                )
+                # valid_proposals[matched_node] = 0
+                # rejections = _vec_binomial(n * [p[t]])
+                # new_rejections = np.bitwise_and(
+                #     rejections,
+                #     valid_proposals
+                # )
    
-                keep_nodes = np.invert(new_rejections)
-                offline_mask = np.bitwise_and(offline_mask, keep_nodes)
+                # keep_nodes = np.invert(new_rejections)
+                # offline_mask = np.bitwise_and(offline_mask, keep_nodes)
     return matching, val
 
 
 def lp_approx(A: _Array, p: _Array, coin_flips: _Array):
     x, _ = lp_match(A, p, verbose=False)
-    # x = _validate_feasibility(x, p)
 
     return online_lp_rounding(x, A, p, coin_flips)